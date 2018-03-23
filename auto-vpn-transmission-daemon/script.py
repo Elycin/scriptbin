7b1db14d071170ba567e3dc659220ea8ce0ef002
Ely Haughie <Elycin2@gmail.com>
2018-03-23 09:37:32 -0400

    Update script.py

--- auto-vpn-transmission-daemon/script.py
+++ auto-vpn-transmission-daemon/script.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-
-"""
-Automatic VPN for Transmission Daemon
-
-This script was created in conjunction with OpenVPN to automatically reconfigure the interface that torrenting will occur on.
-Please configure your settings.json beforehand, as this script only makes changes to the bound ipv4 address\
-"""
-
 import json
 import netifaces as ni
 import os
@@ -16,6 +8,13 @@ config = {
     "transmission_configuration": "/etc/transmission-daemon/settings.json",
     "vpn_interface": "tun0",
 }
+
+"""
+Automatic VPN for Transmission Daemon
+
+This script was created in conjunction with OpenVPN to automatically reconfigure the interface that torrenting will occur on.
+Please configure your settings.json beforehand, as this script only makes changes to the bound ipv4 address\
+"""
 
 def get_transmission_config():
     global config
